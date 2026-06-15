a735ba2ed91de692701a90b00e137399e73db1ad
ViniciusSoles <vinisoles77@gmail.com>
2026-06-15 14:02:29 -0300

    Initial template structure

---
+++ Template.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace Template.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
